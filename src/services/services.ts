8e6f5d54ae813ae6873ef0fe83c9a4514df284ae
EnsarDzaf0 <ensardzafo03@gmail.com>
2024-08-05 11:34:19 +0200

    Switching base url to live be

--- src/services/services.ts
+++ src/services/services.ts
@@ -2,7 +2,7 @@ import axios, { AxiosRequestConfig } from "axios";
 import Cookies from "js-cookie";
 
 //export const baseUrl = 'http://localhost:8080/api';
-export const baseUrl = 'http://159.89.17.33/api'
+export const baseUrl = 'https://medic-lab-api.online/api'
 
 export const loginRoute = '/login';
 export const getAllUsersRoute = '/users';
